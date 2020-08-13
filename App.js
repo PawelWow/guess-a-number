a9ebdb5a1fa43343fb0e37fc42d07ea10f481956
pawel <pawel@webperfect.pl>
2020-08-13 22:24:45 +0200

    game screen logic

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './components/Header';
 import StartGameScreen from './components/screens/StartGameScreen';
+import GameScreen from './components/screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const onStartGame = selectedNumber => {
+    setUserNumber(selectedNumber);
+  }
+
+  // wybiera ekran gry
+  const showGameScreen = () => {
+    if(userNumber) {
+      return <GameScreen userChoice={userNumber} />;
+    }
+  
+    return <StartGameScreen onStartGame={onStartGame} />;
+  };
+
   return (
     <View style={styles.screen}>
       <Header title="Guess a number" />
-      <StartGameScreen /> 
+      {showGameScreen()}
     </View>
   );
 }
