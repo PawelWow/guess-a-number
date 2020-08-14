414246fad00ad1ebf4bb7b003db830dee4629745
pawel <pawel@webperfect.pl>
2020-08-14 16:49:35 +0200

    game logic and game over screen

--- App.js
+++ App.js
@@ -4,20 +4,36 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './components/screens/StartGameScreen';
 import GameScreen from './components/screens/GameScreen';
+import GameOverScreen from './components/screens/GameOverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const onConfigureNewGame = () => {
+      setGuessRounds(0);
+      setUserNumber(null);
+  }
 
   const onStartGame = selectedNumber => {
     setUserNumber(selectedNumber);
   }
 
+  const onGameOver = roundsNumber => {
+    setGuessRounds(roundsNumber);
+  };
+
   // wybiera ekran gry
   const showGameScreen = () => {
-    if(userNumber) {
-      return <GameScreen userChoice={userNumber} />;
+    if(userNumber && guessRounds <= 0 ) {
+      return <GameScreen userChoice={userNumber} onGameOver={onGameOver} />;
     }
   
+    if(guessRounds > 0)
+    {
+      return <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={onConfigureNewGame} />
+    }
+
     return <StartGameScreen onStartGame={onStartGame} />;
   };
 
