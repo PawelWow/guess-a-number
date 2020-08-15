1e2e02c72898c3a0388b906398843e319839965c
pawel <pawel@webperfect.pl>
2020-08-15 15:45:47 +0200

    select platform

--- components/MainButton.ios.js
+++ components/MainButton.ios.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { 
+    View, 
+    Text, 
+    StyleSheet, 
+    TouchableOpacity } from 'react-native';
 
 import Colors from '../constans/colors';
 
@@ -27,4 +31,3 @@ const styles = StyleSheet.create({
 });
 
 export default MainButton;
-
